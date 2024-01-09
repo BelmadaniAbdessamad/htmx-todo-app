e2c1f989d519973df9bd14a3037a7298e13e58f3
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2024-01-10 00:10:44 +0100

    service : get all todos action

--- api.php
+++ api.php
@@ -1,12 +1,20 @@
 
 
 <?php
-
+require_once "todo.php";
 switch ($_GET['action']) {
     case 'post-todo':
-        echo"posting a todo";
+        if (isset($_POST['todo']) &&  $_POST['todo'] != "") {
+            $todo = new Todo($_POST['todo'], false);
+            echo $todo->insertTodo();
+        } else {
+        }
+
         break;
-    
+    case 'todos-list':
+        echo Todo::getTodos();
+        break;
+
     default:
         # code...
         break;
