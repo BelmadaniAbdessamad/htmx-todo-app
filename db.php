e3e7206ff987ba8edc7bf8fc020d71289c56b28b
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2024-01-09 22:40:55 +0100

    refactor : using a singlton database class

--- db.php
+++ db.php
@@ -1,14 +1,27 @@
 <?php 
-$db = null;
-try {
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "todos";
-    
-    // Create a connection to the database
-    $db = new mysqli($host, $username, $password, $database);
-    
-} catch (\Throwable $th) {
-    //throw $th;
+
+
+class Db {
+    private static mysqli $connection;
+    private static $host = "localhost";
+    private static $username = "root";
+    private static $password = "";
+    private static $database = "todos";
+
+    private function __construct()
+    { 
+    }
+
+    public static function getInstance():mysqli
+    {
+        if(Db::$connection==null){
+            try {
+                Db::$connection =new mysqli(Db::$host, Db::$username,Db::$password, Db::$database);
+            } catch (\Throwable $th) {
+                //throw $th;
+            }
+           
+        }
+        return Db::$connection;
+    }
 }
